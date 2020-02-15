cdd992d641428581c7478867dd13d8d549326777
Simon Garner <simon.garner@madscience.co.nz>
2020-02-16 11:52:48 +1300

    Do a merge commit when destination exists

--- github-sync.sh
+++ github-sync.sh
@@ -32,7 +32,16 @@ git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHU
 git remote add tmp_upstream "$UPSTREAM_REPO"
 git fetch tmp_upstream
 git remote -v
-git checkout -b "${DESTINATION_BRANCH}" "refs/remotes/tmp_upstream/${SOURCE_BRANCH}"
+
+if git ls-remote --exit-code --heads origin "${DESTINATION_BRANCH}"; then
+  # When the destination branch already exists, switch to it and merge in the source branch
+  git checkout -b "${DESTINATION_BRANCH}" "refs/remotes/origin/${DESTINATION_BRANCH}"
+  git merge --commit --no-edit -ff "refs/remotes/tmp_upstream/${SOURCE_BRANCH}"
+  # TODO: handle merge conflicts
+else
+  # When the destination does not exist yet, checkout the source branch as destination
+  git checkout -b "${DESTINATION_BRANCH}" "refs/remotes/tmp_upstream/${SOURCE_BRANCH}"
+fi
 
 if [[ ! -z "$EXCLUDED_FILES" ]]; then
   git reset origin/master -- "${EXCLUDED_FILES}"
